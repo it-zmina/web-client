192a48d226e71f2d2d8b6129ea3a5172940d90b4
Alex <userok.zmina@gmail.com>
2021-10-15 23:51:13 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,7 @@
+#Web client
+
+_Template for web client_
+
+## Step 1 -- Clone
+
+1. Clone to your local machine: `git clone `
---
+++ git_settings.sh
@@ -0,0 +1,13 @@
+#dir=${PWD##*/}
+dir=".git"
+echo "$dir"
+if [ -d "$dir" ]; then
+  echo "Set git config"
+  git config --local core.autocrlf input
+  git config --local user.name "<Your name>"
+  git config --local user.email "<Your github email>"
+  git config user.name
+  git config user.email
+else
+    echo "Error: current directory is not project root"
+fi
